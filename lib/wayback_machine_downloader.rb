fa366c14a9c7af6346fef1aa6d14fec6ef6a7e6d
hartator <hartator@gmail.com>
2015-08-09 22:33:32 -0500

    Make file_list_curated aware of timestamp option

--- lib/wayback_machine_downloader.rb
+++ lib/wayback_machine_downloader.rb
@@ -7,7 +7,7 @@ class WaybackMachineDownloader
 
   def initialize params
     @base_url = params[:base_url]
-    @timestamp = params[:timestamp]
+    @timestamp = params[:timestamp].to_i
   end
 
   def backup_name
@@ -23,16 +23,18 @@ class WaybackMachineDownloader
     file_list_curated = Hash.new
     file_list_raw.each_line do |line|
       line = line.split(' ')
-      timestamp = line[1].to_i
+      file_timestamp = line[1].to_i
       file_url = line[2]
       file_id = file_url.split('/')[3..-1].join('/')
       file_id = URI.unescape file_id
-      if file_list_curated[file_id]
-        unless file_list_curated[file_id][:timestamp] > timestamp
-          file_list_curated[file_id] = {file_url: file_url, timestamp: timestamp}
+      if @timestamp == 0 or file_timestamp <= @timestamp
+        if file_list_curated[file_id]
+          unless file_list_curated[file_id][:timestamp] > file_timestamp
+            file_list_curated[file_id] = {file_url: file_url, timestamp: file_timestamp}
+          end
+        else
+          file_list_curated[file_id] = {file_url: file_url, timestamp: file_timestamp}
         end
-      else
-        file_list_curated[file_id] = {file_url: file_url, timestamp: timestamp}
       end
     end
     file_list_curated
@@ -43,7 +45,6 @@ class WaybackMachineDownloader
     puts
     file_list_curated = get_file_list_curated
     file_list_curated.each do |file_id, file_remote_info|
-      timestamp = file_remote_info[:timestamp]
       file_url = file_remote_info[:file_url]
       file_path_elements = file_id.split('/')
       if file_id == ""
@@ -74,7 +75,7 @@ class WaybackMachineDownloader
       end
     end
     puts
-    puts "Download complete, saved in #{backup_path}. (#{file_list_curated.size} files downloaded.)"
+    puts "Download complete, saved in #{backup_path}. (#{file_list_curated.size} files downloaded)"
   end
 
 end
