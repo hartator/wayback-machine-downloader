811a61e852f55855d8fb71f669a919716311bde5
hartator <hartator@gmail.com>
2016-07-31 09:51:35 -0500

    Bump gem version

--- lib/wayback_machine_downloader.rb
+++ lib/wayback_machine_downloader.rb
@@ -8,7 +8,7 @@ require_relative 'wayback_machine_downloader/to_regex'
 
 class WaybackMachineDownloader
 
-  VERSION = "0.4.1"
+  VERSION = "0.4.2"
 
   attr_accessor :base_url, :from_timestamp, :to_timestamp, :only_filter, :exclude_filter, :all
 
