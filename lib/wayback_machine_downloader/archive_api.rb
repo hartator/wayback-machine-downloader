7eedc1a183aa0eed3634e2a07394c8eec0c3cb35
hartator <hartator@gmail.com>
2016-09-24 10:04:57 -0700

    Get snapshot result page per page index

--- lib/wayback_machine_downloader/archive_api.rb
+++ lib/wayback_machine_downloader/archive_api.rb
@@ -1,26 +1,29 @@
 module ArchiveAPI
 
-	def get_raw_list_from_api url
+	def get_raw_list_from_api url, page_index
 		request_url = "http://web.archive.org/cdx/search/xd?url="
 		request_url += url
-		request_url += parameters_for_api
-		request_uri = URI.parse request_url
-		response = Net::HTTP.get_response request_uri
-		response.body
+		request_url += parameters_for_api page_index
+    request_uri = URI.parse request_url
+    response = Net::HTTP.get_response request_uri
+    response.body
 	end
 
-	def parameters_for_api
-		parameters = "&fl=timestamp,original&gzip=false"
+	def parameters_for_api page_index
+		parameters = "&fl=timestamp,original&collapse=digest&gzip=false"
     if @all
-      parameters += "&collapse=digest"
+      parameters += ""
     else
-      parameters += "&filter=statuscode:200&collapse=original"
+      parameters += "&filter=statuscode:200"
     end
     if @from_timestamp and @from_timestamp != 0
       parameters += "&from=" + @from_timestamp.to_s
     end
     if @to_timestamp and @to_timestamp != 0
       parameters += "&to=" + @to_timestamp.to_s
+    end
+    if page_index
+      parameters += "&page=#{page_index}"
     end
     parameters
   end
