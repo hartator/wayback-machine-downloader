7383bdfadc61cda5c645ab2b2fdd67ee724fc968
hartator <hartator@gmail.com>
2016-10-17 12:40:29 -0500

    Bump Gem version

--- lib/wayback_machine_downloader.rb
+++ lib/wayback_machine_downloader.rb
@@ -14,7 +14,7 @@ class WaybackMachineDownloader
 
   include ArchiveAPI
 
-  VERSION = "1.1.2"
+  VERSION = "1.1.3"
 
   attr_accessor :base_url, :directory, :from_timestamp, :to_timestamp, :only_filter, :exclude_filter, :all, :list, :maximum_pages, :threads_count
 
