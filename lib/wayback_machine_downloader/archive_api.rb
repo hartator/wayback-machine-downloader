dc3f6b745445fc5753ca87f09251f90dcd51e085
Max Vasiliev <max@vasiliev.me>
2018-01-17 20:50:31 +0300

    Escape url to support idn domain names

--- lib/wayback_machine_downloader/archive_api.rb
+++ lib/wayback_machine_downloader/archive_api.rb
@@ -5,7 +5,7 @@ module ArchiveAPI
     request_url += url
     request_url += parameters_for_api page_index
 
-    open(request_url).read
+    open(URI.escape(request_url)).read
   end
 
   def parameters_for_api page_index
