30475c5c9e1d92d63b75dc5f22a40dd16c1aa23a
hartator <hartator@gmail.com>
2021-06-06 19:47:11 -0500

    Make URI#open cross Ruby versions compatible

--- lib/wayback_machine_downloader/archive_api.rb
+++ lib/wayback_machine_downloader/archive_api.rb
@@ -10,7 +10,7 @@ module ArchiveAPI
     request_url.query = URI.encode_www_form(params)
 
     begin
-      json = JSON.parse(URI.open(request_url).read)
+      json = JSON.parse(URI(request_url).open.read)
       if (json[0] <=> ["timestamp","original"]) == 0
         json.shift
       end
